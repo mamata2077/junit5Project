cf94ee04bab9f509b65134a9890eceb39343539f
mamata2077 <mamata.r.raote@accenture.com>
2020-12-02 20:27:14 +0530

    initial commit

---
+++ src/test/java/org/practice/Junit5Maven/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package org.practice.Junit5Maven;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+    @Test
+    void firstTest() {
+        System.out.println("First test");
+    }
+}
